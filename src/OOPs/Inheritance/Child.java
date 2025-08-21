80f5fe8c5a493be4dad8d215102d35af9421eabe
Subrat Kumar Bhola <Subrat.Bhola@coforge.com>
2025-08-21 11:27:08 +0530

    inheritance

--- src/OOPs/Inheritance/Child.java
+++ src/OOPs/Inheritance/Child.java
@@ -1,5 +1,29 @@
 package OOPs.Inheritance;
 
-public class Child {
+public class Child extends Parent {
+
+    private String childName;
+
+    public Child(String familyName, String parentName, String childName) {
+        super(familyName, parentName);
+        this.childName = childName;
+    }
+
+    public String getChildName() {
+        return childName;
+    }
+
+    public void setChildName(String childName) {
+        this.childName = childName;
+    }
+
+    @Override
+    public String toString() {
+        return "Child{" +
+                "familyName='" + getFamilyName() + '\'' +
+                ", parentName='" + getParentName() + '\'' +
+                ", childName='" + childName + '\'' +
+                '}';
+    }
     
 }
--- src/OOPs/Inheritance/GrandParent.java
+++ src/OOPs/Inheritance/GrandParent.java
@@ -2,4 +2,24 @@ package OOPs.Inheritance;
 
 public class GrandParent {
     
+    private String familyName;
+
+    public GrandParent(String familyName) {
+        this.familyName = familyName;
+    }
+
+    public String getFamilyName() {
+        return familyName;
+    }
+
+    public void setFamilyName(String familyName) {
+        this.familyName = familyName;
+    }
+
+    @Override
+    public String toString() {
+        return "GrandParent{" +
+                "familyName='" + familyName + '\'' +
+                '}';
+    }
 }
